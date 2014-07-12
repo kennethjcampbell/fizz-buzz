86d9838fab0276cc5c83b4dfb57bea67af562ae6
kennethjcampbell <kennethjcampbell@gmail.com>
2014-07-12 13:17:22 -0400

    Refractor

--- js/app.js
+++ js/app.js
@@ -1,28 +1,42 @@
 $(document).ready(function() {
 
-   $('#center').on('click','button', function() {
-      addItem();
-    });
+  var num = prompt("Enter a Number");
+  num++;
 
-     function addItem() {
-      for (var i = 1; i < 101; i++) {
-        if (i % 15 === 0) {
-          $('ul').append("<li>FizzBuzz</li>");
-        }
-        else if (i % 5 === 0) {
-          $('ul').append("<li>Buzz</li>");
-        }
-        else if (i % 3 === 0) {
-          $('ul').append("<li>Fizz</li>");
-        }
-        else {
-          $('ul').append("<li>" + i + "</li>");
-        }
-     
-      }
+ while (num % 1 != 0) {
+    if (isNaN(num)){
+      alert("This is not a Number");
+      num = prompt("Enter a Number");
+      num++;
     }
-    
+    else {
+      alert("Should be an Integer");
+      num = prompt("Enter a Number");
+      num++;
+    }
+  } 
+
+  $('#center').on('click','button', function() {
+      addItem(num);
+  });
+
 });
 
-
+var addItem = function(number) {
+  for (var i = 1; i < number; i++) {
+    if (i % 15 === 0) {
+      $('ul').append("<li>FizzBuzz</li>");
+    }
+    else if (i % 5 === 0) {
+      $('ul').append("<li>Buzz</li>");
+    }
+    else if (i % 3 === 0) {
+      $('ul').append("<li>Fizz</li>");
+    }
+    else {
+      $('ul').append("<li>" + i + "</li>");
+    }
+     
+  }
+}
 
